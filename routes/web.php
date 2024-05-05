a4c2a669a365702247eb70f6ba927768214a7709
Rachmad Kurniawan <rachmadkurniawan@MacBook-Pro-Rachmad.local>
2024-05-05 08:43:27 +0800

    feat todo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Todo\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(TodoController::class)->group(function() {
+    Route::get('/todo', 'index')->name('todo');
+    Route::post('/todo', 'store')->name('todo.post');
+    Route::put('/todo/{id}', 'update')->name('todo.update');
+    Route::delete('/todo/{id}', 'destroy')->name('todo.delete');
 });
